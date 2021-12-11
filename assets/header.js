cb3940add081be5d02177df40b59859ad89c9815
ankushdogradev <ankushdogradev@gmail.com>
2021-12-11 19:32:33 +0530

    Added nav menu

--- assets/header.js
+++ assets/header.js
@@ -1,6 +1,14 @@
 const anim = document.getElementById("anim");
+const overlay = document.getElementById("overlay");
+const coll = document.getElementById("collection");
+const collUl = document.getElementById("collection--ul")
 
-console.log("Anim: ", anim);
 anim.addEventListener("click", () => {
     anim.classList.toggle("act");
+    overlay.classList.toggle("act");
+});
+
+coll.addEventListener("click", () => {
+    collUl.classList.toggle("col--act");
+    console.log("HEllo")
 });
