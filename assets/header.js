ba3fe62ab865c6b17895bd342b2752408b9e8008
ankushdogradev <ankushdogradev@gmail.com>
2021-12-12 01:34:17 +0530

    added header Links & breakpoint

--- assets/header.js
+++ assets/header.js
@@ -2,6 +2,8 @@ const anim = document.getElementById("anim");
 const overlay = document.getElementById("overlay");
 const coll = document.getElementById("collection");
 const collUl = document.getElementById("collection--ul")
+const hColl = document.getElementById("h_collection");
+const hCollUl = document.getElementById("h_collection--ul")
 
 anim.addEventListener("click", () => {
     anim.classList.toggle("act");
@@ -13,5 +15,13 @@ coll.addEventListener("click", () => {
         collUl.style.display = "none";
     } else if (collUl.style.display = "none") {
         collUl.style.display = "block";
+    }
+})
+
+hColl.addEventListener("click", () => {
+    if (hCollUl.style.display === "block") {
+        hCollUl.style.display = "none";
+    } else if (hCollUl.style.display = "none") {
+        hCollUl.style.display = "block";
     }
 })
